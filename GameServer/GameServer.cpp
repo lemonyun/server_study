62fd1dbe9c1038425a52b7c830c795a33cb76edb
Lemonyun <lemonyun9@naver.com>
2022-09-16 21:55:20 +0900

    Conditional Variable + Producer Consumer 패턴

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -11,17 +11,27 @@ mutex m;
 queue<int32> q;
 HANDLE handle;
 
+// CV는 user level object
+condition_variable cv;
+
 void Producer()
 {
 	while (true)
 	{
+		// 1) Lock을 잡고
+		// 2) 공유 변수 값을 수정
+		// 3) Lock을 풀고
+		// 4) 조건 변수 통해 다른 쓰레드에게 통지
+
 		{
 			unique_lock<mutex> lock(m);
 			q.push(100);
 		}
 
-		::SetEvent(handle); // 핸들의 상태를 Signal 상태로 바꿈
-		this_thread::sleep_for(100ms);
+		cv.notify_one();
+
+		//::SetEvent(handle); // 핸들의 상태를 Signal 상태로 바꿈
+		// this_thread::sleep_for(100ms);
 	}
 }
 
@@ -29,15 +39,18 @@ void Consumer()
 {
 	while (true)
 	{
-		::WaitForSingleObject(handle, INFINITE);
+
+		//::WaitForSingleObject(handle, INFINITE);
 		// auto 이벤트의 경우 깨자마자 Signal이 non signal 상태로 자동으로 바뀜
 		// manual 이벤트의 경우 ::ResetEvent(handle)을 직접 호출해줘야 non signal 상태로 바뀜
 		unique_lock<mutex> lock(m);
+
+		cv.wait(lock, []() {return q.empty() == false; });
 		if (q.empty() == false)
 		{
 			int32 data = q.front();
 			q.pop();
-			cout << data << endl;
+			cout << q.size() << endl;
 		}
 	}
 }
