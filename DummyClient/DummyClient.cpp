25691147e36963983a9b9fca1055cdd1efcdbb2d
Lemonyun <lemonyun9@naver.com>
2022-09-22 20:17:12 +0900

    Select 모델

--- DummyClient/DummyClient.cpp
+++ DummyClient/DummyClient.cpp
@@ -19,15 +19,76 @@ int main()
         return 0;
 
     
-    SOCKET clientSocket = ::socket(AF_INET, SOCK_DGRAM, 0);
+    SOCKET clientSocket = ::socket(AF_INET, SOCK_STREAM, 0);
     if (clientSocket == INVALID_SOCKET)
-    {
-        HandleError("Socket");
         return 0;
+
+    u_long on = 1;
+    if (::ioctlsocket(clientSocket, FIONBIO, &on) == INVALID_SOCKET)
+        return 0;
+
+    SOCKADDR_IN serverAddr;
+    ::memset(&serverAddr, 0, sizeof(serverAddr));
+    serverAddr.sin_family = AF_INET;
+    ::inet_pton(AF_INET, "127.0.0.1", &serverAddr.sin_addr);
+    serverAddr.sin_port = ::htons(7777);
+
+    while (true)
+    {
+        if (::connect(clientSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
+        {
+            if (::WSAGetLastError() == WSAEWOULDBLOCK)
+                continue;
+
+            // 이미 연결중인 상태라면 break
+            if (::WSAGetLastError() == WSAEISCONN)
+                break;
+
+            // Error
+            break;
+        }
     }
 
+    cout << "Connected to Server!" << endl;
 
+    char sendBuffer[100] = "Hello World";
+     
+    // Send 
+    while (true)
+    {
+        if (::send(clientSocket, sendBuffer, sizeof(sendBuffer), 0) == SOCKET_ERROR)
+        {
+            if (::WSAGetLastError() == WSAEWOULDBLOCK)
+                continue;
 
+            // Error
+            break;
+        }
+        cout << "Send Data! Len = " << sizeof(sendBuffer) << endl;
+        
+        while (true)
+        {
+            char recvBuffer[1000];
+            int32 recvLen = ::recv(clientSocket, recvBuffer, sizeof(recvBuffer), 0);
+            if (recvLen == SOCKET_ERROR)
+            {
+                if (::WSAGetLastError() == WSAEWOULDBLOCK)
+                    continue;
+
+                // Error
+                break;
+            }
+            else if (recvLen == 0)
+            {
+                break;
+            }
+
+            cout << "Recv Data Len = " << recvLen << endl;
+            break;
+        }
+
+        this_thread::sleep_for(1s);
+    }
 
     // 소켓 리소스 반환
     ::closesocket(clientSocket);
