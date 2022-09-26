adfc7f83fcb04e0fdc017a0e72377fe696bc5540
Lemonyun <lemonyun9@naver.com>
2022-09-26 16:30:30 +0900

    패킷 직렬화 #2

--- DummyClient/ClientPacketHandler.cpp
+++ DummyClient/ClientPacketHandler.cpp
@@ -26,6 +26,10 @@ void ClientPacketHandler::HandlePacket(BYTE* buffer, int32 len)
 
 // 고정 크기 데이터를 먼저 앞에 몰아놓고 가변적인 데이터들은 뒤에
 // [PKT_S_TEST][BuffsListItem BuffsListItem BuffsListItem]
+
+// 패킷 직렬화의 두 가지 방법
+// 데이터를 전달할 때 임시객체를 사용하는 방법과 사용하지 않는 방법
+// 프로토버퍼는 임시 객체를 사용한다.
 struct PKT_S_TEST
 {
 	struct BuffsListItem
@@ -49,12 +53,25 @@ struct PKT_S_TEST
 		uint32 size = 0;
 
 		size += sizeof(PKT_S_TEST);
+
+		if (packetSize < size)
+			return false;
+
 		size += buffsCount * sizeof(BuffsListItem);
 		if (size != packetSize)
 			return false;
 
 		if (buffsOffset + buffsCount * sizeof(BuffsListItem) > packetSize)
 			return false;
+	}
+
+	using BuffsList = PacketList<PKT_S_TEST::BuffsListItem>;
+
+	BuffsList GetBufferList()
+	{
+		BYTE* data = reinterpret_cast<BYTE*>(this);
+		data += buffsOffset;
+		return BuffsList(reinterpret_cast<PKT_S_TEST::BuffsListItem*>(data), buffsCount);
 	}
 	/*vector<BuffData> buffs;
 	wstring name;*/
@@ -66,29 +83,37 @@ void ClientPacketHandler::Handle_S_TEST(BYTE* buffer, int32 len)
 {
 	BufferReader br(buffer, len); 
 
-	if (len < sizeof(PKT_S_TEST))
-		return;
+	// 임시 객체를 사용하지 않는 방법
+	PKT_S_TEST* pkt = reinterpret_cast<PKT_S_TEST*>(buffer);
 
-	PKT_S_TEST pkt;
-	br >> pkt;
+	// 이렇게 복붙할 필요 없다.
+	//PKT_S_TEST pkt;
+	//br >> pkt;
 	
-	if (pkt.Validate() == false)
+	if (pkt->Validate() == false)
 		return;
 
 	//cout << "ID: " << id << "HP : " << hp << "ATT :" << attack << endl;*/
 
-	vector<PKT_S_TEST::BuffsListItem> buffs;
+	// 이렇게 복붙할 필요 없다.
+	// vector<PKT_S_TEST::BuffsListItem> buffs; 
+	//buffs.resize(pkt->buffsCount);
 
-	buffs.resize(pkt.buffsCount);
+	PKT_S_TEST::BuffsList buffs = pkt->GetBufferList();
 
-	for (int32 i = 0; i < pkt.buffsCount; i++)
-		br >> buffs[i];
+	/*for (int32 i = 0; i < pkt->buffsCount; i++)
+		br >> buffs[i];*/
 
 
-	cout << "BufCount : " << pkt.buffsCount << endl;
+	cout << "BufCount : " << buffs.Count() << endl;
 
-	for (int32 i = 0; i < pkt.buffsCount; i++)
+	for (int32 i = 0; i < buffs.Count(); i++)
 	{
 		cout << "Buf Info : " << buffs[i].buffId << " " << buffs[i].remainTime << endl;
+	}
+
+	for (auto it = buffs.begin(); it != buffs.end(); it++)
+	{
+		cout << "Buf Info : " << it->buffId << " " << it->remainTime << endl;
 	}
 }
