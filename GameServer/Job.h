2022e3688affe69a1dd143a12d53a00c0d05e09e
Lemonyun <lemonyun9@naver.com>
2022-09-29 19:16:03 +0900

    JobQueue #1

---
+++ GameServer/Job.h
@@ -0,0 +1,48 @@
+#pragma once
+class IJob
+{
+public:
+	virtual void Execute() {}
+};
+
+class HealJob : public IJob
+{
+public:
+	virtual void Execute() override
+	{
+		// _targetÀ» Ã£¾Æ¼­
+		// _target0>ADDHP(_headlValue)
+		cout << _target << "ÇÑÅ× Èú" << _healValue << "¸¸Å­ ÁÜ";
+	}
+
+public:
+	uint64 _target = 0;
+	uint32 _healValue = 0;
+};
+
+using JobRef = shared_ptr<IJob>;
+
+class JobQueue
+{
+public:
+	void Push(JobRef job)
+	{
+		WRITE_LOCK;
+		_jobs.push(job);
+	}
+
+	JobRef Pop()
+	{
+		WRITE_LOCK;
+		if (_jobs.empty())
+			return nullptr;
+
+		JobRef ret = _jobs.front();
+		_jobs.pop();
+		return ret;
+	}
+
+private:
+	USE_LOCK;
+	queue<JobRef> _jobs;
+};
